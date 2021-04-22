2b0b2f9d34145d429b2126915dd954f56722fe1c
root <root@ip-172-31-29-71.us-west-2.compute.internal>
2021-04-22 09:15:45 +0000

    commited

---
+++ src/main/java/com/sit/App.java
@@ -0,0 +1,13 @@
+package com.sit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
